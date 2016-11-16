5ea4dd69f064c9dd47d43e4fc32f9b577b9e38fa
bearclaw813 <christopher.redig@gmail.com>
2016-11-15 19:07:13 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+﻿window.sr = ScrollReveal();
+sr.reveal('#site1');
+sr.reveal('#site2');
+sr.reveal('#site3');
+sr.reveal('#site4');
+sr.reveal('#site5');
+sr.reveal('#site6');
+sr.reveal('#beginningSection', { duration: 2000 });
